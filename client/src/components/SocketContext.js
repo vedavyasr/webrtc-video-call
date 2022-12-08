6acb545c57eb1257aeaf1e9fa5317aff3fe43d90
vedavyas <rvn582@gmail.com>
2022-12-08 14:13:46 +0530

    updated BE url

--- client/src/components/SocketContext.js
+++ client/src/components/SocketContext.js
@@ -10,7 +10,7 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("http://localhost:5000");
+const socket = io("https://veda-video-call.herokuapp.com"); //in local http://localhost:5000
 
 const ContextProvider = ({ children }) => {
   const [stream, setStream] = useState(null);
